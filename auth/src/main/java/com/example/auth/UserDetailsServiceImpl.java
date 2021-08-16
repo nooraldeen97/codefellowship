844c1728db1572cf0e4e9c912aa2a582d3172e2c
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-08-16 05:29:35 +0300

    lab16

---
+++ auth/src/main/java/com/example/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+        @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+
+        if(applicationUser == null){
+            throw  new UsernameNotFoundException("The user "+ username + " does not exist");
+        }
+        return applicationUser;
+    }
+}
